a2b94ebdd961c40b2c3f18e223b58ee6a90abe3d
unknown <usuario@gfi05345.gfi.es>
2019-12-13 13:04:08 +0100

    primer commit- pom

---
+++ Selenium/src/test/java/com/henry/Selenium/FlightFinder.java
@@ -0,0 +1,28 @@
+package com.henry.Selenium;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FlightFinder extends BasePage {
+	public FlightFinder(WebDriver driver) {
+		super(driver);
+	}
+	
+	public Boolean checkFlightFinder() {
+		
+		List <WebElement> fonts = driver.findElements(By.tagName("font"));
+		for (WebElement font:fonts) {
+			if (font.getText().startsWith("Use our Flight Finder")){
+				return true;
+			}
+		}
+		return false;
+		
+	}	
+
+}
